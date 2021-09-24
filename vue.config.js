15e90b22d95ef1cd66e1f55509cbb34aa06eb995
bonnv79 <ngovanbon16@gmail.com>
2021-09-25 00:10:42 +0700

    update version 1.1.1

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? process.env.PROJECT_NAME
+    : './',
   configureWebpack: {
     resolve: {
       alias: {
