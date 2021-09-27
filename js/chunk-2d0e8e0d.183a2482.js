ea90a33ced1f4529b6eaf5102dd28b07d1aefcee
bonnv79 <ngovanbon16@gmail.com>
2021-09-27 17:13:09 +0700

    Updates

---
+++ js/chunk-2d0e8e0d.183a2482.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e8e0d"],{"8ac1":function(e,n,c){"use strict";c.r(n);var t=c("7a23");function r(e,n){var c=Object(t["E"])("router-view");return Object(t["w"])(),Object(t["e"])(c)}const o={};o.render=r;n["default"]=o}}]);
+//# sourceMappingURL=chunk-2d0e8e0d.183a2482.js.map
---
+++ js/chunk-7bfe4ab7.9b83cb16.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-7bfe4ab7"],{c48c:function(e,c,n){"use strict";n.r(c);var t=n("7a23");Object(t["z"])("data-v-2ef6a538");var a={class:"root"},o=Object(t["h"])("h1",null,"Page Not Found",-1),r=[o];function s(e,c,n,o,s,u){return Object(t["w"])(),Object(t["g"])("div",a,r)}Object(t["x"])();var u={name:"Page",components:{},props:{}};n("c892");u.render=s,u.__scopeId="data-v-2ef6a538";c["default"]=u},c892:function(e,c,n){"use strict";n("d9be")},d9be:function(e,c,n){}}]);
+//# sourceMappingURL=chunk-7bfe4ab7.9b83cb16.js.map
